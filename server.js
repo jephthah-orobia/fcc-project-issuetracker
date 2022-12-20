ac57edac69089fbf7f7230ce3b8e57e4a6a1507b
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2022-12-20 19:39:33 +0800

    connect server to database

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
 'use strict';
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const expect      = require('chai').expect;
-const cors        = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const expect = require('chai').expect;
+const cors = require('cors');
 require('dotenv').config();
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to database");
+  }).catch(e => {
+    console.error(e);
+    console.log("Failed to connect to database");
+  });
 
 let app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: '*' })); //For FCC testing purposes only
 
 
 
@@ -33,28 +42,30 @@ app.route('/')
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
+
 //For FCC testing purposes
 fccTestingRoutes(app);
 
 //Routing for API 
-apiRoutes(app);  
-    
-//404 Not Found Middleware
-app.use(function(req, res, next) {
+apiRoutes(app);
+
+app.use(function (req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
 });
 
+//404 Not Found Middleware
+
 //Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
+  if (process.env.NODE_ENV === 'test') {
     setTimeout(function () {
+      console.log('Running Tests...');
       try {
         runner.run();
-      } catch(e) {
+      } catch (e) {
         console.log('Tests are not valid:');
         console.error(e);
       }
