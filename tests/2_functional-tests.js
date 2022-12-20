2473813e22efe9a8e543f94e8e6dc0038bfe9369
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2022-12-20 17:28:15 +0800

    created test for api: POST /api/issues/{project}
    Create an issue with every field: POST request to /api/issues/{project}

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,29 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-  
+suite('Functional Tests', function () {
+    test('Create an issue with every field: POST request to /api/issues/{project}', function (done) {
+        chai.request(server)
+            .post('/api/issues/someproject', {
+                issue_title: "some title",
+                issue_text: "Some text",
+                created_by: "some user",
+                assigned_to: "someone",
+                status_text: "some status text"
+            }).end(function (err, res) {
+                assert.equal(res.status, 200);
+                let resjson;
+                assert.doesNotThrow(() => resjson = JSON.parse(res.text));
+                assert.deepPropertyVal(resjson, "issue_title", "some title");
+                assert.deepPropertyVal(resjson, "issue_text", "some text");
+                assert.deepPropertyVal(resjson, "issue_user", "some user");
+                assert.deepPropertyVal(resjson, "assign_to", "someone");
+                assert.deepPropertyVal(resjson, "status_text", "some status text");
+                assert.deepPropertyVal(resjson, "open", true);
+                assert.deepPropertyVal(resjson, "created_on");
+                assert.deepPropertyVal(resjson, "updated_on");
+                assert.deepProperty(resjson, "_id");
+                done();
+            });
+    });
 });
