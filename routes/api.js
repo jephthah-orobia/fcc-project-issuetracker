e4c57699c6a7fcc56022d14ec5faf0fff53e07bf
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2022-12-20 21:30:40 +0800

    Local Testing, passed.

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 'use strict';
+const { Error } = require('mongoose');
 const Issue = require('../datastore/issueSchema');
 const Project = require('../datastore/projectSchema');
 
@@ -9,7 +10,7 @@ module.exports = function (app) {
     .get(async function (req, res) {
       let project = req.params.project;
       Project.findOne({ name: project })
-        .populate('issues')
+        .populate({ path: 'issues', match: req.query })
         .exec((err, proj) => {
           if (err)
             res.send(err);
@@ -29,19 +30,21 @@ module.exports = function (app) {
         assigned_to: req.body.assigned_to || '',
         status_text: req.body.status_text || ''
       }).save((err, issue) => {
-        if (err)
-          res.send(err);
+        if (err && err instanceof Error.ValidationError)
+          res.json({ error: 'required field(s) missing' });
+        else if (err)
+          res.json({ error: '' + err });
         else
           Project.findOne({ name: project }, (err1, proj) => {
             if (err1)
-              res.send(err1);
+              res.json({ error: err1 + '' });
             else if (!proj)
               new Project({
                 name: project,
                 issues: issue._id
               }).save((err2, newProj) => {
                 if (err2)
-                  res.send(err2)
+                  res.json({ error: err2 + '' })
                 else
                   res.json(issue);
               });
@@ -49,7 +52,7 @@ module.exports = function (app) {
               proj.issues.push(issue._id);
               proj.save((err3, doc) => {
                 if (err3)
-                  res.send(err3);
+                  res.json({ error: err3 + '' });
                 else
                   res.json(issue);
 
@@ -60,13 +63,56 @@ module.exports = function (app) {
     })
 
     .put(function (req, res) {
-      let project = req.params.project;
-
+      if (!req.body._id) {
+        res.json({ error: 'missing _id' });
+        return;
+      }
+      let countfields = 0, to_update = {};
+      for (let prop in req.body)
+        if (prop !== '_id') {
+          to_update[prop] = req.body[prop]
+          countfields++;
+        }
+      if (countfields == 0) {
+        res.json({
+          error: 'no update field(s) sent',
+          _id: req.body._id
+        });
+        return;
+      }
+      Issue.updateOne(
+        { _id: req.body._id },
+        to_update,
+        (err) => {
+          if (err)
+            res.json({
+              error: 'could not update',
+              _id: req.body._id
+            });
+          else
+            res.json({
+              result: 'successfully updated',
+              _id: req.body._id
+            });
+        });
     })
 
     .delete(function (req, res) {
-      let project = req.params.project;
-
+      if (!req.body._id)
+        res.json({ error: 'missing _id' });
+      else
+        Issue.deleteOne({ _id: req.body._id }, (err) => {
+          if (err)
+            res.json({
+              error: 'could not delete',
+              _id: req.body._id
+            });
+          else
+            res.json({
+              result: 'successfully deleted',
+              _id: req.body._id
+            });
+        });
     });
 
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,6 +6,7 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function () {
+    let _id;
     test('Create an issue with every field: POST request to /api/issues/{project}', function (done) {
         chai.request(server)
             .post('/api/issues/someproject')
@@ -19,15 +20,16 @@ suite('Functional Tests', function () {
                 assert.equal(res.status, 200);
                 let resjson;
                 assert.doesNotThrow(() => resjson = JSON.parse(res.text));
-                assert.deepPropertyVal(resjson, "issue_title", "some title");
-                assert.deepPropertyVal(resjson, "issue_text", "some text");
-                assert.deepPropertyVal(resjson, "created_by", "some user");
-                assert.deepPropertyVal(resjson, "assigned_to", "someone");
-                assert.deepPropertyVal(resjson, "status_text", "some status text");
-                assert.deepPropertyVal(resjson, "open", true);
-                assert.property(resjson, "created_on", res.text);
+                assert.propertyVal(resjson, "issue_title", "some title");
+                assert.propertyVal(resjson, "issue_text", "some text");
+                assert.propertyVal(resjson, "created_by", "some user");
+                assert.propertyVal(resjson, "assigned_to", "someone");
+                assert.propertyVal(resjson, "status_text", "some status text");
+                assert.propertyVal(resjson, "open", true);
+                assert.property(resjson, "created_on");
                 assert.property(resjson, "updated_on");
                 assert.property(resjson, "_id");
+                _id = resjson._id;
                 done();
             });
     });
@@ -43,13 +45,13 @@ suite('Functional Tests', function () {
                 assert.equal(res.status, 200);
                 let resjson;
                 assert.doesNotThrow(() => resjson = JSON.parse(res.text));
-                assert.deepPropertyVal(resjson, "issue_title", "some title");
-                assert.deepPropertyVal(resjson, "issue_text", "some text");
-                assert.deepPropertyVal(resjson, "created_by", "some user");
-                assert.deepPropertyVal(resjson, "assigned_to", "");
-                assert.deepPropertyVal(resjson, "status_text", "");
-                assert.deepPropertyVal(resjson, "open", true);
-                assert.property(resjson, "created_on", res.text);
+                assert.propertyVal(resjson, "issue_title", "some title");
+                assert.propertyVal(resjson, "issue_text", "some text");
+                assert.propertyVal(resjson, "created_by", "some user");
+                assert.propertyVal(resjson, "assigned_to", "");
+                assert.propertyVal(resjson, "status_text", "");
+                assert.propertyVal(resjson, "open", true);
+                assert.property(resjson, "created_on");
                 assert.property(resjson, "updated_on");
                 assert.property(resjson, "_id");
                 done();
@@ -65,9 +67,205 @@ suite('Functional Tests', function () {
                 assert.equal(res.status, 200);
                 let resjson;
                 assert.doesNotThrow(() => resjson = JSON.parse(res.text));
-                assert.property(resjson, "error");
                 assert.propertyVal(resjson, "error", "required field(s) missing");
                 done();
+            });
+    });
+
+    test('View issues on a project: GET request to /api/issues/{project}', function (done) {
+        chai.request(server)
+            .get('/api/issues/someject')
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                let resjson;
+                assert.doesNotThrow(() => resjson = JSON.parse(res.text));
+                assert.isArray(resjson);
+                assert.isAtLeast(resjson.length, 0);
+                if (resjson.length > 0) {
+                    assert.deepProperty('_id');
+                    assert.deepPropertyVal(resjson, "issue_title", "some title");
+                    assert.deepPropertyVal(resjson, "issue_text", "some text");
+                    assert.deepPropertyVal(resjson, "created_by", "some user");
+                    assert.deepPropertyVal(resjson, "assigned_to", "");
+                    assert.deepPropertyVal(resjson, "status_text", "");
+                    assert.deepPropertyVal(resjson, "open", true);
+                    assert.deepProperty(resjson, "created_on");
+                    assert.deepProperty(resjson, "updated_on");
+                }
+                done();
+            });
+    });
+
+    test('View issues on a project with one filter: GET request to /api/issues/{project}', function (done) {
+        chai.request(server)
+            .get('/api/issues/someject')
+            .send({ open: false })
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                let resjson;
+                assert.doesNotThrow(() => resjson = JSON.parse(res.text));
+                assert.isArray(resjson);
+                assert.isAtLeast(resjson.length, 0);
+                if (resjson.length > 0) {
+                    assert.deepProperty('_id');
+                    assert.deepPropertyVal(resjson, "issue_title", "some title");
+                    assert.deepPropertyVal(resjson, "issue_text", "some text");
+                    assert.deepPropertyVal(resjson, "created_by", "some user");
+                    assert.deepPropertyVal(resjson, "assigned_to", "");
+                    assert.deepPropertyVal(resjson, "status_text", "");
+                    assert.deepPropertyVal(resjson, "open", true);
+                    assert.deepProperty(resjson, "created_on");
+                    assert.deepProperty(resjson, "updated_on");
+                }
+                done();
+            });
+    });
+
+    test('View issues on a project with multiple filters: GET request to /api/issues/{project}', function (done) {
+        chai.request(server)
+            .get('/api/issues/someject')
+            .send({ open: false, created_by: "some user" })
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                let resjson;
+                assert.doesNotThrow(() => resjson = JSON.parse(res.text));
+                assert.isArray(resjson);
+                assert.isAtLeast(resjson.length, 0);
+                if (resjson.length > 0) {
+                    assert.deepProperty('_id');
+                    assert.deepPropertyVal(resjson, "issue_title", "some title");
+                    assert.deepPropertyVal(resjson, "issue_text", "some text");
+                    assert.deepPropertyVal(resjson, "created_by", "some user");
+                    assert.deepPropertyVal(resjson, "assigned_to", "");
+                    assert.deepPropertyVal(resjson, "status_text", "");
+                    assert.deepPropertyVal(resjson, "open", true);
+                    assert.deepProperty(resjson, "created_on");
+                    assert.deepProperty(resjson, "updated_on");
+                }
+                done();
+            });
+    });
+
+
+    test('Update one field on an issue: PUT request to /api/issues/{project}', function (done) {
+        chai.request(server)
+            .put('/api/issues/someproject')
+            .send({
+                _id: _id,
+                open: false
+            }).end(function (err, res) {
+                assert.equal(res.status, 200);
+                let resjson;
+                assert.doesNotThrow(() => resjson = JSON.parse(res.text));
+                assert.propertyVal(resjson, "result", "successfully updated");
+                assert.propertyVal(resjson, "_id", _id);
+                done();
+            });
+    });
+
+    test('Update multiple fields on an issue: PUT request to /api/issues/{project}', function (done) {
+        chai.request(server)
+            .put('/api/issues/someproject')
+            .send({
+                _id: _id,
+                open: true,
+                status_text: "there was some changes and needs to be reopened"
+            }).end(function (err, res) {
+                assert.equal(res.status, 200);
+                let resjson;
+                assert.doesNotThrow(() => resjson = JSON.parse(res.text));
+                assert.propertyVal(resjson, "result", "successfully updated");
+                assert.propertyVal(resjson, "_id", _id);
+                done();
+            });
+    });
+
+    test('Update an issue with missing _id: PUT requrest to /api/issues/{project}', function (done) {
+        chai.request(server)
+            .put('/api/issues/someproject')
+            .send({
+                open: false
+            }).end(function (err, res) {
+                assert.equal(res.status, 200);
+                let resjson;
+                assert.doesNotThrow(() => resjson = JSON.parse(res.text));
+                assert.propertyVal(resjson, 'error', 'missing _id');
+                assert.notProperty(resjson, "_id");
+                done();
+            });
+    });
+
+    test('Update an issue with no fields to update: PUT requrest to /api/issues/{project}', function (done) {
+        chai.request(server)
+            .put('/api/issues/someproject')
+            .send({
+                _id: _id
+            }).end(function (err, res) {
+                assert.equal(res.status, 200);
+                let resjson;
+                assert.doesNotThrow(() => resjson = JSON.parse(res.text));
+                assert.propertyVal(resjson, 'error', 'no update field(s) sent');
+                assert.propertyVal(resjson, "_id", _id);
+                done();
+            });
+    });
+
+    test('Update an issue with an invalid _id: PUT requrest to /api/issues/{project}', function (done) {
+        chai.request(server)
+            .put('/api/issues/someproject')
+            .send({
+                _id: 'abc',
+                open: false,
+                status_text: 'some changes'
+            }).end(function (err, res) {
+                assert.equal(res.status, 200);
+                let resjson;
+                assert.doesNotThrow(() => resjson = JSON.parse(res.text));
+                assert.propertyVal(resjson, 'error', 'could not update');
+                assert.propertyVal(resjson, "_id", 'abc');
+                done();
+            });
+    });
+
+    test('Delete an issue: DELETE request to /api/issues/{project}', function (done) {
+        chai.request(server)
+            .delete('/api/issues/someproject')
+            .send({
+                _id: _id
+            }).end(function (err, res) {
+                assert.equal(res.status, 200);
+                let resjson;
+                assert.doesNotThrow(() => resjson = JSON.parse(res.text));
+                assert.propertyVal(resjson, 'result', 'successfully deleted');
+                assert.propertyVal(resjson, "_id", _id);
+                done()
+            });
+    });
+
+    test('Delete an issue with an invalid _id: DELETE request to /api/issues/{project}', function (done) {
+        chai.request(server)
+            .delete('/api/issues/someproject')
+            .send({
+                _id: '02139s3a'
+            }).end(function (err, res) {
+                assert.equal(res.status, 200);
+                let resjson;
+                assert.doesNotThrow(() => resjson = JSON.parse(res.text));
+                assert.propertyVal(resjson, 'error', 'could not delete');
+                assert.propertyVal(resjson, "_id", '02139s3a');
+                done()
+            });
+    });
+
+    test('Delete an issue with missing _id: DELETE request to /api/issues/{project}', function (done) {
+        chai.request(server)
+            .delete('/api/issues/someproject')
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                let resjson;
+                assert.doesNotThrow(() => resjson = JSON.parse(res.text));
+                assert.propertyVal(resjson, 'error', 'missing _id');
+                done()
             });
     });
 });
