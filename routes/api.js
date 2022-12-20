e675a20bc2aabbb7288d98a9aa9d098dd11df02f
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2022-12-20 19:40:22 +0800

    created test for possible post to /api/issues/:project

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,72 @@
 'use strict';
+const Issue = require('../datastore/issueSchema');
+const Project = require('../datastore/projectSchema');
 
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+
+    .get(async function (req, res) {
       let project = req.params.project;
-      
+      Project.findOne({ name: project })
+        .populate('issues')
+        .exec((err, proj) => {
+          if (err)
+            res.send(err);
+          else if (!proj)
+            res.json([]);
+          else
+            res.json(proj.issues);
+        });
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let project = req.params.project;
-      
+      new Issue({
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to || '',
+        status_text: req.body.status_text || ''
+      }).save((err, issue) => {
+        if (err)
+          res.send(err);
+        else
+          Project.findOne({ name: project }, (err1, proj) => {
+            if (err1)
+              res.send(err1);
+            else if (!proj)
+              new Project({
+                name: project,
+                issues: issue._id
+              }).save((err2, newProj) => {
+                if (err2)
+                  res.send(err2)
+                else
+                  res.json(issue);
+              });
+            else {
+              proj.issues.push(issue._id);
+              proj.save((err3, doc) => {
+                if (err3)
+                  res.send(err3);
+                else
+                  res.json(issue);
+
+              });
+            }
+          });
+      });
     })
-    
-    .put(function (req, res){
+
+    .put(function (req, res) {
       let project = req.params.project;
-      
+
     })
-    
-    .delete(function (req, res){
+
+    .delete(function (req, res) {
       let project = req.params.project;
-      
+
     });
-    
+
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -8,9 +8,10 @@ chai.use(chaiHttp);
 suite('Functional Tests', function () {
     test('Create an issue with every field: POST request to /api/issues/{project}', function (done) {
         chai.request(server)
-            .post('/api/issues/someproject', {
+            .post('/api/issues/someproject')
+            .send({
                 issue_title: "some title",
-                issue_text: "Some text",
+                issue_text: "some text",
                 created_by: "some user",
                 assigned_to: "someone",
                 status_text: "some status text"
@@ -20,13 +21,52 @@ suite('Functional Tests', function () {
                 assert.doesNotThrow(() => resjson = JSON.parse(res.text));
                 assert.deepPropertyVal(resjson, "issue_title", "some title");
                 assert.deepPropertyVal(resjson, "issue_text", "some text");
-                assert.deepPropertyVal(resjson, "issue_user", "some user");
-                assert.deepPropertyVal(resjson, "assign_to", "someone");
+                assert.deepPropertyVal(resjson, "created_by", "some user");
+                assert.deepPropertyVal(resjson, "assigned_to", "someone");
                 assert.deepPropertyVal(resjson, "status_text", "some status text");
                 assert.deepPropertyVal(resjson, "open", true);
-                assert.deepPropertyVal(resjson, "created_on");
-                assert.deepPropertyVal(resjson, "updated_on");
-                assert.deepProperty(resjson, "_id");
+                assert.property(resjson, "created_on", res.text);
+                assert.property(resjson, "updated_on");
+                assert.property(resjson, "_id");
+                done();
+            });
+    });
+
+    test('Create an issue with only required fields: POST request to /api/issues/{project}', function (done) {
+        chai.request(server)
+            .post('/api/issues/someproject')
+            .send({
+                issue_title: "some title",
+                issue_text: "some text",
+                created_by: "some user"
+            }).end(function (err, res) {
+                assert.equal(res.status, 200);
+                let resjson;
+                assert.doesNotThrow(() => resjson = JSON.parse(res.text));
+                assert.deepPropertyVal(resjson, "issue_title", "some title");
+                assert.deepPropertyVal(resjson, "issue_text", "some text");
+                assert.deepPropertyVal(resjson, "created_by", "some user");
+                assert.deepPropertyVal(resjson, "assigned_to", "");
+                assert.deepPropertyVal(resjson, "status_text", "");
+                assert.deepPropertyVal(resjson, "open", true);
+                assert.property(resjson, "created_on", res.text);
+                assert.property(resjson, "updated_on");
+                assert.property(resjson, "_id");
+                done();
+            });
+    });
+
+    test('Create an issue with missing required fields: POST request to /api/issues/{project}', function (done) {
+        chai.request(server)
+            .post('/api/issues/someproject')
+            .send({
+                issue_title: "some title",
+            }).end(function (err, res) {
+                assert.equal(res.status, 200);
+                let resjson;
+                assert.doesNotThrow(() => resjson = JSON.parse(res.text));
+                assert.property(resjson, "error");
+                assert.propertyVal(resjson, "error", "required field(s) missing");
                 done();
             });
     });
